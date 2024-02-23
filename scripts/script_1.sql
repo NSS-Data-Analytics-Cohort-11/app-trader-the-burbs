3fe7456f7ad0fd900d882ad4146ea6e35368aa57
Jordan-D-Taylor <tayjyat@gmail.com>
2024-02-22 22:44:53 -0500

    one more after 2/22 class

--- scripts/script_1.sql
+++ scripts/script_1.sql
@@ -10,7 +10,7 @@ UNION ALL
 SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,(CAST(REPLACE(price, '$', '') AS NUMERIC)),AVG(review_count) AS review_count_avg,content_rating,genres, 'google' AS app_store
 FROM public.play_store_apps
 WHERE rating >=3.5
-AND price <= '1.00'
+AND (CAST(REPLACE(price, '$', '') AS NUMERIC)) <= '1.00'
 AND name IN 
 	(SELECT name
 		FROM public.app_store_apps)
