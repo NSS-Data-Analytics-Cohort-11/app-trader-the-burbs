8a072aa0b74cb0482a77c6af844118e0c5428d8b
Jordan-D-Taylor <tayjyat@gmail.com>
2024-02-22 22:19:37 -0500

    push end of thursday

--- scripts/script_1.sql
+++ scripts/script_1.sql
@@ -1,16 +1,19 @@
-SELECT *
+SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,price,AVG(CAST(review_count AS INT)) AS review_count_avg,content_rating,primary_genre, 'apple' AS app_store
 FROM public.app_store_apps
-SELECT *
-FROM public.play_store_apps
-
-SELECT name,size_bytes,rating,price,CAST(review_count AS INT),content_rating,primary_genre,'apple' AS app_store
-FROM public.app_store_apps
+WHERE rating >=3.5
+AND price <= '1.00'
+AND name IN 
+	(SELECT name
+		FROM public.play_store_apps)
+GROUP BY name, price, content_rating, primary_genre
 UNION ALL
-SELECT name,size,rating,CAST(REPLACE(price, '$', '')::NUMERIC),review_count,content_rating,genres,'google' AS app_store
-FROM public.play_store_apps;
+SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,(CAST(REPLACE(price, '$', '') AS NUMERIC)),AVG(review_count) AS review_count_avg,content_rating,genres, 'google' AS app_store
+FROM public.play_store_apps
+WHERE rating >=3.5
+AND price <= '1.00'
+AND name IN 
+	(SELECT name
+		FROM public.app_store_apps)
+GROUP BY name, price, content_rating, genres
+ORDER BY name;
 
-SELECT name,review_count,
-       CAST(review_count AS INT) AS numeric_review_count
-FROM app_store_apps
-where rating >= 4.5
-order by numeric_review_count desc
