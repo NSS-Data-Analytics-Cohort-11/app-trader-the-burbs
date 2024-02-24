92f1990540e1272eecbaf611365897f112e1158a
Jordan-D-Taylor <tayjyat@gmail.com>
2024-02-24 11:59:52 -0500

    final push, leaving for my son's birthday party

--- scripts/script_1.sql
+++ scripts/script_1.sql
@@ -1,19 +1,63 @@
-SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,price,AVG(CAST(review_count AS INT)) AS review_count_avg,content_rating,primary_genre, 'apple' AS app_store
+-- SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,price,AVG(CAST(review_count AS INT)) AS review_count_avg,content_rating,primary_genre, 'apple' AS app_store
+-- FROM public.app_store_apps
+-- WHERE rating >=3.5
+-- AND price <= '1.00'
+-- AND name IN 
+-- 	(SELECT name
+-- 		FROM public.play_store_apps)
+-- GROUP BY name, price, content_rating, primary_genre
+-- UNION ALL
+-- SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,(CAST(REPLACE(price, '$', '') AS NUMERIC)),AVG(review_count) AS review_count_avg,content_rating,genres, 'google' AS app_store
+-- FROM public.play_store_apps
+-- WHERE rating >=3.5
+-- AND (CAST(REPLACE(price, '$', '') AS NUMERIC)) <= '1.00'
+-- AND name IN 
+-- 	(SELECT name
+-- 		FROM public.app_store_apps)
+-- GROUP BY name, price, content_rating, genres
+-- ORDER BY name;
+
+-- SELECT name, ROUND((ROUND(AVG(avg_rating)*2,0)/2),2) AS avg_app_rating_combined, ROUND(AVG(review_count_avg),2) AS avg_review_count
+-- FROM 
+-- (SELECT name, AVG(rating) AS avg_rating, price, AVG(CAST(review_count AS INT)) AS review_count_avg, content_rating, primary_genre, 'apple' AS app_store
+-- FROM public.app_store_apps
+-- WHERE rating >=3.5
+-- AND price <= '1.00'
+-- AND name IN 
+-- 	(SELECT name
+-- 		FROM public.play_store_apps)
+-- GROUP BY name, price, content_rating, primary_genre
+-- UNION ALL
+-- SELECT name, AVG(rating) AS avg_rating, (CAST(REPLACE(price, '$', '') AS NUMERIC)), AVG(review_count) AS review_count_avg, content_rating, genres, 'google' AS app_store
+-- FROM public.play_store_apps
+-- WHERE rating >=3.5
+-- AND (CAST(REPLACE(price, '$', '') AS NUMERIC)) <= '1.00'
+-- AND name IN 
+-- 	(SELECT name
+-- 		FROM public.app_store_apps)
+-- GROUP BY name, price, content_rating, genres
+-- ORDER BY name)
+-- GROUP BY name
+-- ORDER BY avg_app_rating_combined DESC, avg_review_count DESC;
+
+SELECT name, ROUND((ROUND(AVG(avg_rating)*2,0)/2),2) AS avg_app_rating_combined, ROUND(AVG(review_count_avg),2) AS avg_review_count, price
+FROM 
+(SELECT name, AVG(rating) AS avg_rating, price, AVG(CAST(review_count AS INT)) AS review_count_avg, content_rating, primary_genre, 'apple' AS app_store
 FROM public.app_store_apps
-WHERE rating >=3.5
-AND price <= '1.00'
-AND name IN 
+WHERE name IN 
 	(SELECT name
 		FROM public.play_store_apps)
 GROUP BY name, price, content_rating, primary_genre
 UNION ALL
-SELECT name,(ROUND(AVG(rating)*2,0)/2) AS avg_rating,(CAST(REPLACE(price, '$', '') AS NUMERIC)),AVG(review_count) AS review_count_avg,content_rating,genres, 'google' AS app_store
+SELECT name, AVG(rating) AS avg_rating, (CAST(REPLACE(price, '$', '') AS NUMERIC)), AVG(review_count) AS review_count_avg, content_rating, genres, 'google' AS app_store
 FROM public.play_store_apps
-WHERE rating >=3.5
-AND (CAST(REPLACE(price, '$', '') AS NUMERIC)) <= '1.00'
-AND name IN 
+WHERE name IN 
 	(SELECT name
 		FROM public.app_store_apps)
 GROUP BY name, price, content_rating, genres
-ORDER BY name;
-
+ORDER BY name)
+WHERE price <= '1.00'
+GROUP BY name, price
+HAVING ROUND((ROUND(AVG(avg_rating)*2,0)/2),2) >=3.5
+ORDER BY avg_app_rating_combined DESC, avg_review_count DESC
+LIMIT 10;
