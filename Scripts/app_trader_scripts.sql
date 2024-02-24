da9bdcdee0f530a12447fed035aa0d5d5343e914
McNuttJ <mcnuttj34@gmail.com>
2024-02-24 14:30:49 -0600

    finished query and project

--- Scripts/app_trader_scripts.sql
+++ Scripts/app_trader_scripts.sql
@@ -65,7 +65,7 @@ order by avg_numeric_review_count desc
 "Asphalt 8: Airborne"
 "PicsArt Photo Studio: Collage Maker & Pic Editor"*/
 
-Select *
+Select name REPLACE(name,'™','') AS name
 FROM app_store_apps
 WHERE name iLIKE '%8%b%'
 
@@ -75,14 +75,14 @@ WHERE name iLIKE '%8%b%'
 
 SELECT name, rating, CAST(price AS MONEY), ROUND(AVG(CAST(review_count AS INT)),2), 'apple' AS app_store
 FROM app_store_apps
-WHERE rating >= 4.5 AND price <= '1.00' AND name IN
+WHERE CAST(price AS MONEY) <= '1.00' AND rating >= 4.5 AND  name IN
 	(SELECT name
 	FROM play_store_apps)
 GROUP BY name, rating, price
 UNION ALL
 SELECT name, rating, CAST(price AS MONEY), ROUND(AVG(CAST(review_count AS INT)),2), 'google' AS app_store
 FROM play_store_apps
-WHERE rating >= 4.5 AND price <= '1.00' AND name IN 
+WHERE CAST(price AS MONEY) <= '1.00' AND rating >= 4.5 AND name IN 
 	(SELECT name
 	FROM app_store_apps)
 GROUP BY name, rating, price
@@ -99,6 +99,69 @@ What demographic?
 Top paid apps?
 What apps have the most downloads?
 Marketing expense (1,000*12)*lifespan of app*/
+
+--Trying replace on name to get 8 ball app.
+SELECT name, rating, CAST(price AS MONEY), ROUND(AVG(CAST(review_count AS INT)),2), primary_genre, 'apple' AS app_store
+FROM app_store_apps
+WHERE CAST(price AS MONEY) <= '1.00' AND rating >= 4.5 AND  name IN
+	(SELECT name
+	FROM play_store_apps)
+GROUP BY name, rating, price, primary_genre
+UNION ALL
+SELECT name, rating, CAST(price AS MONEY), ROUND(AVG(CAST(review_count AS INT)),2), genres, 'google' AS app_store
+FROM play_store_apps
+WHERE CAST(price AS MONEY) <= '1.00' AND rating >= 4.5 AND name IN 
+	(SELECT name
+	FROM app_store_apps)
+GROUP BY name, rating, price, genres
+ORDER BY name, rating
+
+-- Updated query to show combined average app rating bewteen app stores with more liberal rating value to capture more apps.
+SELECT name, (ROUND(AVG(avg_rating)*2,0)/2) AS avg_app_rating_combined
+FROM
+(SELECT name, AVG(rating) AS avg_rating, price, AVG(CAST(review_count AS INT)) AS review_count_avg, content_rating, primary_genre, 'apple' AS app_store
+FROM public.app_store_apps
+WHERE rating >=3.5
+AND price <= '1.00'
+AND name IN
+	(SELECT name
+		FROM public.play_store_apps)
+GROUP BY name, price, content_rating, primary_genre
+UNION ALL
+SELECT name, AVG(rating) AS avg_rating, (CAST(REPLACE(price, '$', '') AS NUMERIC)), AVG(review_count) AS review_count_avg, content_rating, genres, 'google' AS app_store
+FROM public.play_store_apps
+WHERE rating >=3.5
+AND (CAST(REPLACE(price, '$', '') AS NUMERIC)) <= '1.00'
+AND name IN
+	(SELECT name
+		FROM public.app_store_apps)
+GROUP BY name, price, content_rating, genres
+ORDER BY name)
+GROUP BY name
+ORDER BY avg_app_rating_combined DESC, name;
+
+--final query showing
+SELECT name, ROUND((ROUND(AVG(avg_rating)*2,0)/2),2) AS avg_app_rating_combined, ROUND(AVG(review_count_avg),2) AS avg_review_count, price
+FROM
+(SELECT name, AVG(rating) AS avg_rating, price, AVG(CAST(review_count AS INT)) AS review_count_avg, content_rating, primary_genre, 'apple' AS app_store
+FROM public.app_store_apps
+WHERE name IN
+	(SELECT name
+		FROM public.play_store_apps)
+GROUP BY name, price, content_rating, primary_genre
+UNION ALL
+SELECT name, AVG(rating) AS avg_rating, (CAST(REPLACE(price, '$', '') AS NUMERIC)), AVG(review_count) AS review_count_avg, content_rating, genres, 'google' AS app_store
+FROM public.play_store_apps
+WHERE name IN
+	(SELECT name
+		FROM public.app_store_apps)
+GROUP BY name, price, content_rating, genres
+ORDER BY name)
+WHERE price <= '1.00'
+GROUP BY name, price
+HAVING ROUND((ROUND(AVG(avg_rating)*2,0)/2),2) >=3.5
+ORDER BY avg_app_rating_combined DESC, avg_review_count DESC
+LIMIT 10;
 
 
 
